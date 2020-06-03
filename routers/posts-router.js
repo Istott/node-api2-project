afb5e5b15f6ec132a26440097d9aea203ab691d7
isaac stott <isaac.cloyd@gmail.com>
2020-06-02 19:57:12 -0600

    got put working, mvp woot woot

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -74,24 +74,24 @@ router.delete("/:id", (req, res) => {
     });
 });
 
-// router.put("/:id", (req, res) => {
-//   const changes = req.body;
-//   Hubs.update(req.params.id, changes)
-//     .then(hub => {
-//       if (hub) {
-//         res.status(200).json(hub);
-//       } else {
-//         res.status(404).json({ message: "The hub could not be found" });
-//       }
-//     })
-//     .catch(error => {
-//       // log error to database
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error updating the hub",
-//       });
-//     });
-// });
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "The post could not be found" });
+      }
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the post",
+      });
+    });
+});
 
 module.exports = router;
 
